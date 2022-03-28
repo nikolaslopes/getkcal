4d069ce760962157fd659254e808172239449d9e
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-03-28 01:02:51 -0400

    :fire: adjusted const tmb

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,22 @@ function handleSubmit(event) {
   const weight = getInputNumberValue('weight');
   const height = getInputNumberValue('height');
   const activityLevel = getSelectedValue('activity_level');
+
+  // * taxa metabólica basal
+  const tmb = (
+    gender === 'female'
+      ? (655 + (9.6 * weight) + (1.8 * height) - (4.7 * age))
+      : (66 + (13.7 * weight) + (5 * height) - (6.8 * age))
+  );
+
+  const maintenance = Math.round(tmb * Number(activityLevel));
+  const loseWeight = maintenance - 450;
+  const gainWeight = maintenance + 450;
 }
 
 function getSelectedValue(id) {
   const select = document.getElementById(id);
-  return select.options[select.selectedIndex];
+  return select.options[select.selectedIndex].value;
 }
 
 function getInputNumberValue(id) {
