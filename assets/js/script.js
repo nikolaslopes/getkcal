97780d0a2ad3135ae9de17c38051bc16924a38f2
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-03-28 00:42:23 -0400

    :fire: submit fields finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 const form = document.getElementById('form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const gender = getSelectedValue('gender');
+  const age = getInputNumberValue('age');
+  const weight = getInputNumberValue('weight');
+  const height = getInputNumberValue('height');
+  const activityLevel = getSelectedValue('activity_level');
+}
+
+function getSelectedValue(id) {
+  const select = document.getElementById(id);
+  return select.options[select.selectedIndex];
+}
+
+function getInputNumberValue(id) {
+  return Number(document.getElementById(id).value);
+}
